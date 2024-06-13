c2eafb066f25f0a49cfffc99cdb2a00937902aaa
sailalith <lalithvoop@hotmail.com>
2024-06-14 01:46:08 +0530

    enabled all components and commented component var in tfvars

--- main.tf
+++ main.tf
@@ -1,29 +1,29 @@
 module "frontend" {
   source = "./modules/app"
   instance_type = var.instance_type
-  component = var.component
+  component = "frontend"
   os_pass = var.os_pass
   os_user = var.os_user
   env = var.env
   zone_id = var.zone_id
 }
 
-#module "backend" {
-#  source = "./modules/app"
-#  instance_type = var.instance_type
-#  component = var.component
-#  os_pass = var.os_pass
-#  os_user = var.os_user
-#  env = var.env
-#  zone_id = var.zone_id
-#}
-#
-#module "mysql" {
-#  source = "./modules/app"
-#  instance_type = var.instance_type
-#  component = var.component
-#  os_pass = var.os_pass
-#  os_user = var.os_user
-#  env = var.env
-#  zone_id = var.zone_id
-#}
+module "backend" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "backend"
+  os_pass = var.os_pass
+  os_user = var.os_user
+  env = var.env
+  zone_id = var.zone_id
+}
+
+module "mysql" {
+  source = "./modules/app"
+  instance_type = var.instance_type
+  component = "mysql"
+  os_pass = var.os_pass
+  os_user = var.os_user
+  env = var.env
+  zone_id = var.zone_id
+}
