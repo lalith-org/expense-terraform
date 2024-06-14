b34d9d2afae447ba905a130eab575038a5f01b8b
sailalith <lalithvoop@hotmail.com>
2024-06-14 22:56:31 +0530

    updated backend

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "terraform-bucket-1306"
-    key = "expense-terraform/dev/tools"
+    key = "expense-terraform/dev/state"
     region = "us-east-1"
   }
 }
