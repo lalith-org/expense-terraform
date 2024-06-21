fe89ff70a4f2d53e449b0e6e29737e61d07c7dfc
sailalith <lalithvoop@hotmail.com>
2024-06-21 12:28:36 +0530

    changing role_name to role

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-module "frontend" {
-  depends_on = [module.backend]
-  source = "./modules/app"
-  instance_type = var.instance_type
-  component = "frontend"
-  env = var.env
-  zone_id = var.zone_id
-  vault_token = var.vault_token
-}
+#module "frontend" {
+#  depends_on = [module.backend]
+#  source = "./modules/app"
+#  instance_type = var.instance_type
+#  component = "frontend"
+#  env = var.env
+#  zone_id = var.zone_id
+#  vault_token = var.vault_token
+#}
 
 module "backend" {
   depends_on = [module.mysql]
