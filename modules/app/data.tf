172b6d0ddb573fb2d24dae87fe4f7791e57df4d4
sailalith <lalithvoop@hotmail.com>
2024-06-26 22:16:39 +0530

    enabled ansible steps in app setup

--- modules/app/data.tf
+++ modules/app/data.tf
@@ -4,10 +4,6 @@ data "aws_ami" "example" {
   owners           = ["973714476881"]
 }
 
-#data "aws_security_group" "selected" {
-#  name = "allow-all"
-#}
-##
-#data "vault_generic_secret" "ssh_creds" {
-#  path = "common/common"
-#}
+data "vault_generic_secret" "ssh_creds" {
+  path = "common/common"
+}
--- modules/app/main.tf
+++ modules/app/main.tf
@@ -43,22 +43,22 @@ resource "aws_route53_record" "domain" {
   records = [aws_instance.vm.private_ip]
 }
 
-#resource "null_resource" "null1" {
-#  provisioner "remote-exec" {
-#    connection {
-#      type     = "ssh"
-#      user     = jsondecode(data.vault_generic_secret.ssh_creds.data_json).ansible_user
-#      password = jsondecode(data.vault_generic_secret.ssh_creds.data_json).ansible_password
-#      host     = aws_instance.vm.public_ip
-#    }
-#
-#    inline = [
-#      "sudo pip3.11 install ansible hvac",
-#      "sudo pip3.11 install ansible-core",
-#      "ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible.git get-secrets.yml -e env=${var.env} -e role=${var.component}  -e vault_token=${var.vault_token}",
-#      "ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible.git ansible.yml -e env=${var.env} -e role=${var.component} -e @secrets.json -e @app.json",
-#      "sudo rm -rf *"
-#      #"ansible-pull -U https://github.com/lalith2211/expense-ansible.git ansible.yml -i localhost, -e env=${var.env} -e role=${var.component}",
-#    ]
-#  }
-#}
+resource "null_resource" "null1" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = jsondecode(data.vault_generic_secret.ssh_creds.data_json).ansible_user
+      password = jsondecode(data.vault_generic_secret.ssh_creds.data_json).ansible_password
+      host     = aws_instance.vm.public_ip
+    }
+
+    inline = [
+      "sudo pip3.11 install ansible hvac",
+      "sudo pip3.11 install ansible-core",
+      "ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible.git get-secrets.yml -e env=${var.env} -e role=${var.component}  -e vault_token=${var.vault_token}",
+      "ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible.git ansible.yml -e env=${var.env} -e role=${var.component} -e @secrets.json -e @app.json",
+      "sudo rm -rf *"
+      #"ansible-pull -U https://github.com/lalith2211/expense-ansible.git ansible.yml -i localhost, -e env=${var.env} -e role=${var.component}",
+    ]
+  }
+}
