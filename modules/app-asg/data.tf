40120f241634f617acc6f2d1b08244fd7fbc079a
sailalith <lalithvoop@hotmail.com>
2024-07-20 17:21:41 +0530

    change of ami and userdata

--- modules/app-asg/data.tf
+++ modules/app-asg/data.tf
@@ -1,7 +1,9 @@
 data "aws_ami" "example" {
   most_recent      = true
-  name_regex       = "RHEL-9-DevOps-Practice"
-  owners           = ["973714476881"]
+#  name_regex       = "RHEL-9-DevOps-Practice"
+#  owners           = ["973714476881"]
+  name_regex  = "golden-ami-*"
+  owners      = ["self"]
 }
 
 data "vault_generic_secret" "ssh" {
--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-pip3.11 install ansible hvac &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible get-secrets.yml -e env=${env} -e role=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
 ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible ansible.yml -e env=${env} -e role=${component} -e @~/secrets.json &>>/opt/ansible.log
