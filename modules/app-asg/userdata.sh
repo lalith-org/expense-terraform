8f981df421a78e3167893bc7a9299b23c2239e9a
sailalith <lalithvoop@hotmail.com>
2024-07-29 23:23:58 +0530

    running only get-secrets

--- modules/app-asg/userdata.sh
+++ modules/app-asg/userdata.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+
 ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible get-secrets.yml -e env=${env} -e role=${component}  -e vault_token=${vault_token} &>>/opt/ansible.log
-ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible ansible.yml -e env=${env} -e role=${component} -e @~/secrets.json &>>/opt/ansible.log
+#ansible-pull -i localhost, -U https://github.com/lalith2211/expense-ansible ansible.yml -e env=${env} -e role=${component} -e @~/secrets.json &>>/opt/ansible.log
